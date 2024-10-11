2574a9fb1b5c528421d90b731639cee1a9075831
samvillasmith <samuel.villa.smith@hotmail.com>
2024-10-10 20:55:30 -0500

    project setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
   )
 }
 
